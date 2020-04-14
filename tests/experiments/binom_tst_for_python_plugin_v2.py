ea9171464cd53cf99705f60b703ff74405f70112
Rohit Pandey <rohitpandey576@gmail.com>
2020-04-13 19:32:23 -0700

    Update binom_tst_for_python_plugin_v2.py

--- tests/experiments/binom_tst_for_python_plugin_v2.py
+++ tests/experiments/binom_tst_for_python_plugin_v2.py
@@ -1,7 +1,7 @@
 from scipy.stats import binom_test
 result=df
 result["p_value"] = result.apply(lambda row : binom_test(row.Events_B,row.Events_A + row.Events_B,row.Uptime_B/(row.Uptime_A +row.Uptime_B),alternative='greater'), axis=1)
-result["p_value_two_sided"] = result.apply(lambda row : binom_test(row.Events_B,row.Events_A + row.Events_B,row.Uptime_B/(row.Uptime_A +row.Uptime_B)), axis=1 )
+result["p_value_two_sided"] = result.apply(lambda row : binom_test_v2(row.Events_B,row.Events_A + row.Events_B,row.Uptime_B/(row.Uptime_A +row.Uptime_B)), axis=1 )
 
 def binom_test_v2(x, n=None, p=0.5, alternative='two-sided'):
     n = np.int_(n)
