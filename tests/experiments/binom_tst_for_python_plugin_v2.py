fbe2695460b040871671f49e2cc382bbc40cd571
Rohit Pandey <rohitpandey576@gmail.com>
2020-04-13 19:25:12 -0700

    Update binom_tst_for_python_plugin_v2.py

--- tests/experiments/binom_tst_for_python_plugin_v2.py
+++ tests/experiments/binom_tst_for_python_plugin_v2.py
@@ -110,5 +110,5 @@ def binary_search(a, d, lo, hi, asc_order=False):
 
 from scipy.stats import binom_test
 result=df
-result["p_value"] = result.apply(lambda row : binom_test_v2(row.Events_B,row.Events_A + row.Events_B,row.Uptime_B/(row.Uptime_A +row.Uptime_B),alternative='greater'), axis=1)
-result["p_value_two_sided"] = result.apply(lambda row : binom_test_v2(row.Events_B,row.Events_A + row.Events_B,row.Uptime_B/(row.Uptime_A +row.Uptime_B)), axis=1 )
+result["p_value"] = result.apply(lambda row : binom_test(row.Events_B,row.Events_A + row.Events_B,row.Uptime_B/(row.Uptime_A +row.Uptime_B),alternative='greater'), axis=1)
+result["p_value_two_sided"] = result.apply(lambda row : binom_test(row.Events_B,row.Events_A + row.Events_B,row.Uptime_B/(row.Uptime_A +row.Uptime_B)), axis=1 )
